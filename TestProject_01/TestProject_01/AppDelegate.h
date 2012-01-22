43f7c0be8d69c18fe3daddfc0960b26c484b17fc
Grifter7 <viper7bj@hotmail.com>
2012-01-21 21:42:33 -0700

    commit

---
+++ TestProject_01/TestProject_01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject_01
+//
+//  Created by Lion User on 21/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ TestProject_01/TestProject_01/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TestProject_01
+//
+//  Created by Lion User on 21/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
