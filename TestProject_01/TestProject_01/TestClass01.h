48e2f51b596c045a0b43de72e4dcadea7175ad8c
Grifter7 <viper7bj@hotmail.com>
2012-01-21 21:45:36 -0700

    added files

---
+++ TestProject_01/TestProject_01/TestClass01.h
@@ -0,0 +1,13 @@
+//
+//  TestClass01.h
+//  TestProject_01
+//
+//  Created by Lion User on 21/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass01 : NSObject
+
+@end
---
+++ TestProject_01/TestProject_01/TestClass01.m
@@ -0,0 +1,13 @@
+//
+//  TestClass01.m
+//  TestProject_01
+//
+//  Created by Lion User on 21/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestClass01.h"
+
+@implementation TestClass01
+
+@end
